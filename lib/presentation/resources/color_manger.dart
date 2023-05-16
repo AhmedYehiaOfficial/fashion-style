0fffaaf475ed1f7b8902b0f9a12d4487c54947d7
dell <ahmedyehiaofficial>
2023-05-16 16:07:59 +0300

    app core

---
+++ lib/presentation/resources/color_manger.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class ColorManger {
+  static Color primary = const Color(0xff000000);
+  static Color backGround = const Color(0xffE5E5E5);
+  static Color darkGrey = const Color(0xff666666);
+  static Color grey = const Color(0xff737477);
+  static Color lightGrey = const Color(0xffF3F4F5);
+}
---
+++ lib/presentation/resources/strings_manger.dart
@@ -0,0 +1,12 @@
+class AppStrings{
+  static const noRouteFound = "No Route found";
+  static const onBoardingTitle1 = "20% Discount New Arrival Product";
+  static const onBoardingTitle2 = "Take Advantage Of The Offer Shopping ";
+  static const onBoardingTitle3 = "All Types Offers Within Your Reach";
+
+  static const onBoardingSubTitle = "Publish up your selfies to make yourself more beautiful with this app.";
+
+  static const skip = "Skip";
+
+
+}
