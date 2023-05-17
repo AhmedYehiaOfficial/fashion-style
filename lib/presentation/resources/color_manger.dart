15ef73d41540c0aa4b58bd58f53393befe6ae22b
HEND_HAMED\hendhamed <hendhamedmorsy@gmail.com>
2023-05-17 21:35:03 +0300

    update app core

--- lib/presentation/resources/color_manger.dart
+++ lib/presentation/resources/color_manger.dart
@@ -1,10 +1,11 @@
 import 'dart:ui';
 
 class ColorManger {
-  static Color primary = const Color(0xff000000);
-  static Color backGround = const Color(0xffE5E5E5);
-  static Color darkGrey = const Color(0xff666666);
-  static Color grey = const Color(0xff737477);
-  static Color grey2 = const Color(0xffEEEEEE);
-  static Color lightGrey = const Color(0xffF3F4F5);
+
+  static Color primaryColor = const Color(0xff000000);
+  static Color backgroundColor = const Color(0xffE5E5E5);
+  static Color darkGreyColor = const Color(0xff666666);
+  static Color greyColor = const Color(0xff737477);
+  static Color grey2Color = const Color(0xffEEEEEE);
+  static Color lightGreyColor = const Color(0xffF3F4F5);
 }
--- lib/presentation/resources/theme_manger.dart
+++ lib/presentation/resources/theme_manger.dart
@@ -1,32 +1,28 @@
 import 'package:fashion/presentation/resources/color_manger.dart';
 import 'package:flutter/material.dart';
 
-ThemeData getApplicationTheme(){
+ThemeData getApplicationTheme() {
   return ThemeData(
-    // main colors
-    primaryColor: ColorManger.primary,
+      primaryColor: ColorManger.primaryColor,
+      scaffoldBackgroundColor: ColorManger.backgroundColor,
+      elevatedButtonTheme: getElevatedButtonTheme(),
+      appBarTheme: AppBarTheme(
+        backgroundColor: ColorManger.backgroundColor,
+      ),
+      buttonTheme: const ButtonThemeData()
 
+      // input decoration theme (tff)
 
-    //card view theme
-
-
-
-
-    //app bar theme
-appBarTheme: AppBarTheme(
-  backgroundColor: ColorManger.backGround,
-
-),
-
-
-
-    //buttom theme
-buttonTheme: const ButtonThemeData(
-
-)
-
-
-    // input decoration theme (tff)
-
-  );
+      );
 }
+
+ElevatedButtonThemeData? getElevatedButtonTheme() => ElevatedButtonThemeData(
+      style: ButtonStyle(
+        textStyle: MaterialStateProperty.all(TextStyle()),
+        foregroundColor: MaterialStatePropertyAll(ColorManger.primaryColor),
+        elevation: const MaterialStatePropertyAll(0),
+        shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        )),
+      ),
+    );
